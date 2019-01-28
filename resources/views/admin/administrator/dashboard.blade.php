cc61e7c56d359d2fdc3c567e7524379109f0570d
Fabian Zapata <fabian@g12media.net>
2019-01-27 22:36:23 -0500

    Update one three 12

--- resources/views/admin/administrator/dashboard.blade.php
+++ resources/views/admin/administrator/dashboard.blade.php
@@ -5,7 +5,7 @@
     <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
   </head>
   <body>
-    <video id="preview"></video>
+    <video id="preview" style='width:100%'></video>
     <script type="text/javascript">
       let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
@@ -13,6 +13,7 @@
       });
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
+          console.error(cameras);
           scanner.start(cameras[0]);
         } else {
           console.error('No cameras found.');
