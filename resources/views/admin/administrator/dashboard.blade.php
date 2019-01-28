fcd2c1aacd84796242860c2f4c113101bb92e91b
Fabian Zapata <fabian@g12media.net>
2019-01-28 10:26:09 -0500

    update application 10

--- resources/views/admin/administrator/dashboard.blade.php
+++ resources/views/admin/administrator/dashboard.blade.php
@@ -5,12 +5,11 @@
     <script type="text/javascript" src="{{URL::asset('https://rawgit.com/schmich/instascan-builds/master/instascan.min.js')}}"></script>
   </head>
   <body>
-    <video id="preview" style="width:100%;"></video>
+    <video id="preview" style="width:80%;"></video>
     <script type="text/javascript">
-      let scanner = new Instascan.Scanner({ video: document.getElementById('preview'),  mirror: false });
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
-        console.log(content);
-        window.open(content,'_self')
+        alert(content);
       });
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
@@ -21,6 +20,6 @@
       }).catch(function (e) {
         console.error(e);
       });
-    </script>
+      </script>
   </body>
 </html>
