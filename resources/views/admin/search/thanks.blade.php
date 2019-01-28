ccfadb565f5d709a903594efc4fcdd742c527688
Fabian Zapata <fabian@g12media.net>
2019-01-27 22:50:28 -0500

    Update one three 17

--- resources/views/admin/search/thanks.blade.php
+++ resources/views/admin/search/thanks.blade.php
@@ -11,15 +11,15 @@ data-animsition-out="fade-out">>
 
           <div class="col-md-12">
               <div class="form-group" style="width: 70%;">
-                <img src="{{URL::asset('admin/assets/images/pet-icon.png')}}" alt="..." class="img-profile-f">
+
                 <h2 style="color:white;">Registro Exitoso!</h2>
               </div>
               <div class="form-group" style="width: 70%;">
-                  <p>Es una maravillosa noticia para la comunidad cuando alguien encuentra una mascota</p>
+                  <p>Se realizo el registro de la actividad.</p>
               </div>
               <div class="form-group" style="width: 70%;">
                 <br>
-                  <button type="submit" onclick="window.location.href='/'" class="btn btn-primary">
+                  <button type="submit" onclick="window.location.href='/administrator'" class="btn btn-primary">
                      Volver al inicio
                   </button>
               </div>
