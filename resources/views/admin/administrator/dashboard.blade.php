a28685442bc2c93a32387e5a46332331e27ab62b
Fabian Zapata <fabian@g12media.net>
2019-01-29 19:01:16 -0500

    Application Data

--- resources/views/admin/administrator/dashboard.blade.php
+++ resources/views/admin/administrator/dashboard.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>QR Scan</title>
-    <script type="text/javascript" src="{{URL::asset('https://rawgit.com/schmich/instascan-builds/master/instascan.min.js')}}"></script>
+    <script type="text/javascript" src="{{URL::asset('admin/qr/instascan.min.js')}}"></script>
   </head>
   <body>
     <video id="preview" style="width:80%;"></video>
