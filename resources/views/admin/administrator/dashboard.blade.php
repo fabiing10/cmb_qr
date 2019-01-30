ea7c51d0285df8b19cf35e4ea20b04dbc0f4832c
Fabian Zapata <fabian@g12media.net>
2019-01-29 19:17:05 -0500

    Application Data camara 6

--- resources/views/admin/administrator/dashboard.blade.php
+++ resources/views/admin/administrator/dashboard.blade.php
@@ -8,14 +8,14 @@
     <span id="id_cam"></span>
     <video id="preview" style="width:100%;"></video>
     <script type="text/javascript">
-      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
       scanner.addListener('scan', function (content) {
         alert(content);
       });
       Instascan.Camera.getCameras().then(function (cameras) {
 
         if (cameras.length > 0) {
-          scanner.start(cameras);
+          scanner.start(cameras[0]);
         } else {
           console.error('No cameras found.');
         }
