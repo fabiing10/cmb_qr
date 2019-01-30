c33e8cab95d0e59fa4e08e967d285e01c07e2455
Fabian Zapata <fabian@g12media.net>
2019-01-29 19:25:00 -0500

    Application Data camara 10

--- resources/views/admin/administrator/dashboard.blade.php
+++ resources/views/admin/administrator/dashboard.blade.php
@@ -8,20 +8,47 @@
     <span id="id_cam"></span>
     <video id="preview" style="width:100%;"></video>
     <script type="text/javascript">
-      let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
-      scanner.addListener('scan', function (content) {
-        window.open(content, '_self');
-      });
-      Instascan.Camera.getCameras().then(function (cameras) {
 
-        if (cameras.length > 0) {
-          scanner.start(cameras[1]);
-        } else {
-          console.error('No cameras found.');
-        }
-      }).catch(function (e) {
-        console.error(e);
-      });
+    var ua = navigator.userAgent.toLowerCase();
+var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+  // Do something!
+  let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
+  scanner.addListener('scan', function (content) {
+    window.open(content, '_self');
+  });
+  Instascan.Camera.getCameras().then(function (cameras) {
+
+    if (cameras.length > 0) {
+      scanner.start(cameras[1]);
+    } else {
+      console.error('No cameras found.');
+    }
+  }).catch(function (e) {
+    console.error(e);
+  });
+}else{
+  let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: true });
+  scanner.addListener('scan', function (content) {
+    window.open(content, '_self');
+  });
+  Instascan.Camera.getCameras().then(function (cameras) {
+
+    if (cameras.length > 0) {
+      scanner.start(cameras[0]);
+    } else {
+      console.error('No cameras found.');
+    }
+  }).catch(function (e) {
+    console.error(e);
+  });
+}
+
+
+    /* Code for Android
+
+
+      */
       </script>
   </body>
 </html>
